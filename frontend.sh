a766220ae4aa1b25dff77f76804e4bbf4bd95abd
GaneshS <ganvic123@gmail.com>
2024-11-12 13:14:00 +0530

    shell

--- frontend.sh
+++ frontend.sh
@@ -49,3 +49,8 @@ VALIDATE $? "Downloading front application code"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>$LOG_FILE
 VALIDATE $? "Extracting frontend application code"
+
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+VALIDATE $? "Copied expense conf"
+
+systemctl restart nginx
